f6b16dd37a286db12c2d2add1dd08cd2a4b710da
Nguyễn Anh Quân <167054493+aquan17@users.noreply.github.com>
2024-04-15 12:47:19 +0700

    Update t2.js

--- t2.js
+++ t2.js
@@ -1,34 +1,34 @@
-function fadeIn(){
-    var text = $('.content').text().replace(/^\s+|\s+$/g,'');
+function fadeIn() {
+    var text = $('.content').text().replace(/^\s+|\s+$/g, '');
     var length = text.length;
     var i = 0;
     var txt;
     var html = [];
     var sp = 4;
-    for(;i<length;i+=sp) {
-        txt = text.substring(i,i+sp);
+    for (; i < length; i += sp) {
+        txt = text.substring(i, i + sp);
         html.push('<span class="c animated">' + txt + '</span>');
     }
     $('.content').removeClass('c').html(html.join(''));
 
-    for(i = 0,length = html.length;i<length;i++) {
-        !function(i){
-            setTimeout(function(){
+    for (i = 0, length = html.length; i < length; i++) {
+        !function(i) {
+            setTimeout(function() {
                 $('.content').find('.animated').eq(i).addClass('fadeIn');
-            },i*400);
+            }, i * 400);
         }(i);
     }
-};
-
-
-
-document.querySelector(".content").onclick=()=>{
-    document.querySelector("#heart").hidden=false
-    document.querySelector("body").style.backgroundColor="#542246"
-    fadeIn();
-    sound.play(); 
 }
-var sound = new Howl({
-    src: ['ssstik.io_1712586792929.mp3'], // Thay urls thành src
-    loop: true
-});
+
+document.querySelector(".content").onclick = () => {
+    document.querySelector("#heart").hidden = false;
+    document.querySelector("body").style.backgroundColor = "#542246";
+    setTimeout(function() {
+        fadeIn(); // Hiển thị chữ sau độ trễ
+    }, 2800); // Thời gian độ trễ tính bằng mili-giây (ở đây là 1 giây)
+    var sound = new Howl({
+        src: ['ssstik.io_1712586792929.mp3'], // Thay urls thành src
+        loop: true
+    });
+    sound.play(); // Phát âm nhạc
+};
